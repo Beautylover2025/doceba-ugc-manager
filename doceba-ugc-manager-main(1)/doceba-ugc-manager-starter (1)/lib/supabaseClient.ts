049efaf55abdad1dc11eec9495a4c4bdefa603b8
Beautylover2025 <rayyabo@doceba.com>
2025-09-01 19:34:57 +0200

    Delete doceba-ugc-manager-main(1)/doceba-ugc-manager-starter (1) directory

--- doceba-ugc-manager-main(1)/doceba-ugc-manager-starter (1)/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-'use client'
-
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-
-// Kein expliziter Type! Inferenz passt hier perfekt.
-export const supabase = createClientComponentClient()
